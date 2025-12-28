df92add45d0649db32cb96195c039d9a2dc24116
OBones <obones@free.fr>
2025-12-28 17:04:16 +0100

    feat: display the tracking pixel

--- include/trace_activity.h
+++ include/trace_activity.h
@@ -2,6 +2,19 @@
 
 class TraceActivity : public Activity
 {
+private:
+    int8_t lastPitchBin = 0;
+    int8_t lastRollBin = 0;
+    uint32_t pitchRepeat = 0;
+    uint32_t rollRepeat = 0;
+
+    uint8_t x;
+    uint8_t y;
+
+    uint32_t lastMillis;
+
+    void updateDiscretization(float angle, int8_t& lastBin, uint32_t& repeat);
+    void updateCoordinate(int8_t lastBin, uint32_t& repeat, uint8_t axisSize, uint8_t& coordinate);
 public:
     TraceActivity();
 
--- src/trace_activity.cpp
+++ src/trace_activity.cpp
@@ -5,6 +5,7 @@
 #include "BMI160Gen.h"
 
 #include "trace_activity.h"
+#include "led_display.h"
 
 #ifndef M_PI
 #define M_PI (3.14159265358979323846)
@@ -12,15 +13,66 @@
 
 #define ACCEL_SENSITIVITY 16384.0 // Sensitivity for ±2g in LSB/g (adjust based on your configuration)
 
+constexpr uint8_t requiredRepeats[] = {0, 20, 15, 10, 5, 3, 1};
+constexpr uint8_t binCount = sizeof(requiredRepeats) / sizeof(requiredRepeats[0]);
+
 TraceActivity::TraceActivity()
 {
     // auto-calibrate accelerometer
     printf("Calibrating accelerometer...");
+    fflush(stdout);
     BMI160.setRegister(BMI160_RA_CMD, 0x37);
     delay(1000);
-    printf("Done.\n");
+    printf(" Done.\n");
 
     //BMI160.setAccelRate(0b1000);
+
+    x = Display::Width / 2;
+    y = Display::Height / 2;
+}
+
+void TraceActivity::updateDiscretization(float angle, int8_t& lastBin, uint32_t& repeat)
+{
+    // discretize -80;80 into the bins defined above.
+    constexpr float maxAngle = 80;
+    if (angle > maxAngle)
+        angle = maxAngle;
+    else if (angle < -maxAngle)
+        angle = -maxAngle;
+
+    int8_t bin = angle / maxAngle * (binCount - 1);
+
+    if (bin != lastBin)
+    {
+        lastBin = bin;
+        repeat = 0;
+    }
+    else
+    {
+        repeat++;
+    }
+
+    //printf("angle: %.2f°, bin: %d, repeat: %d\n", double(angle), bin, repeat);
+}
+
+void TraceActivity::updateCoordinate(int8_t lastBin, uint32_t& repeat, uint8_t axisSize, uint8_t& coordinate)
+{
+    uint8_t requiredRepeat = requiredRepeats[abs(lastBin)];
+    if (requiredRepeat != 0 && repeat > requiredRepeat)
+    {
+        repeat = 0;
+
+        if (lastBin > 0)
+        {
+            if (coordinate != axisSize - 1)
+                coordinate++;
+        }
+        else
+        {
+            if (coordinate != 0)
+                coordinate--;
+        }
+    }
 }
 
 void TraceActivity::loop()
@@ -28,37 +80,9 @@ void TraceActivity::loop()
     int16_t ax, ay, az;
 
     // Read accelerometer data
-    /*uint8_t buffer[6] = {};
-
-    _i2c_req_t req;
-    req.addr = 0b1101000;
-    req.i2c = MXC_I2C0_BUS0;
-    req.tx_buf = buffer;
-    req.tx_len = 1;
-    req.rx_buf = buffer;
-    req.rx_len = 6;
-
-    buffer[0] = 0x12; // first accelerometer register
-
-    int err = MXC_I2C_MasterTransaction(&req);
-    switch (err)
-    {
-        case E_NO_ERROR:
-            break;
-        case E_BUSY:
-            printf("x");
-            return;
-        default:
-            printf("TraceActivity::loop(): I2C failed: %d.\n", err);
-            return;
-    }
-
-    ax = ((int16_t)buffer[1]) << 8 | buffer[0];
-    ay = ((int16_t)buffer[3]) << 8 | buffer[2];
-    az = ((int16_t)buffer[5]) << 8 | buffer[4];
-
-    /*/BMI160.getAcceleration(&ax, &ay, &az);
-    printf("ax: %d, ay: %d, az: %d\n", ax, ay, az);
+    // Inspired by https://how2electronics.com/interfacing-bmi160-accelerometer-gyroscope-with-arduino/
+    BMI160.getAcceleration(&ax, &ay, &az);
+    //printf("ax: %d, ay: %d, az: %d\n", ax, ay, az);
 
     // Convert raw accelerometer values to g
     float ax_g = ax / ACCEL_SENSITIVITY;
@@ -70,8 +94,26 @@ void TraceActivity::loop()
     float roll = atan2(-ax_g, az_g) * 180.0 / M_PI;
 
     // Print tilt angles
-    printf("Pitch: %.2f°, Roll: %.2f°\n", double(pitch), double(roll));
+    //printf("Pitch: %.2f°, Roll: %.2f°\n", double(pitch), double(roll));
 
-    delay(500);
+    updateDiscretization(pitch, lastPitchBin, pitchRepeat);
+    updateDiscretization(roll, lastRollBin, rollRepeat);
 
+    updateCoordinate(lastPitchBin, pitchRepeat, Display::Width, x);
+    updateCoordinate(lastRollBin, rollRepeat, Display::Height, y);
+
+    //printf("x: %d, y: %d\n", x, y);
+
+    if (millis() - lastMillis > 250)
+    {
+        for(int ledIndex = 0; ledIndex < Display::Height * Display::Width; ledIndex++)
+            Display::leds[ledIndex].fadeToBlackBy(32);
+
+        lastMillis = millis();
+    }
+
+    Display::setPixel(x, y, CRGB::Blue4);
+    Display::update();
+
+    //delay(250);
 }
